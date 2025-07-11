4b0134a3b6440eaed470e74ee56873eec5e35b11
Cheryl Yu <cheryl.yu.oz@icloud.com>
2025-07-11 11:34:24 -0400

    no idea what i have done. but set up my UI perhaps??!

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,20 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Cheryl Yu on 11/7/2025.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+    
+}
